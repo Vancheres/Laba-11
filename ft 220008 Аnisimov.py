85b2f6b6ddb2359c9ae8f9e66e5716434404678c
Vancheres <anisii840@gmail.com>
2024-02-21 09:28:41 +0500

    search

--- ft 220008 Аnisimov.py
+++ ft 220008 Аnisimov.py
@@ -3,3 +3,57 @@ import os
 
 # Настройка логирования
 logging.basicConfig(filename='countries.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+# Словарь стран и столиц
+COUNTRIES = {
+    "Россия": "Москва",
+    "Украина": "Киев",
+    "Беларусь": "Минск",
+    "Казахстан": "Нур-Султан",
+    "Узбекистан": "Ташкент",
+    "Таджикистан": "Душанбе",
+    "Кыргызстан": "Бишкек",
+    "Туркменистан": "Ашхабад",
+    "Азербайджан": "Баку",
+    "Армения": "Ереван",
+}
+
+# Функция поиска столицы
+def get_capital(country):
+    """
+    Функция поиска столицы по названию страны.
+
+    Args:
+        country: Название страны.
+
+    Returns:
+        Столица страны или сообщение о том, что страна не найдена.
+    """
+
+    logging.info('Поиск столицы для страны "{}"'.format(country))
+    if country in COUNTRIES:
+        capital = COUNTRIES[country]
+        logging.info('Столица страны "{}": {}'.format(country, capital))
+        return capital
+    else:
+        logging.warning('Страна "{}" не найдена.'.format(country))
+        return 'Страна "{}" не найдена.'.format(country)
+
+# Функция поиска страны
+def get_country(capital):
+        """
+        Функция поиска страны по названию столицы.
+
+        Args:
+            capital: Название столицы.
+
+        Returns:
+            Страна или сообщение о том, что столица не найдена.
+        """
+
+        logging.info('Поиск страны для столицы "{}"'.format(capital))
+        for country, capital_name in COUNTRIES.items():
+            if capital_name == capital:
+                logging.info('Страна для столицы "{}": {}'.format(capital, country))
+                return country
+        logging.warning('Столица "{}" не найдена.'.format(capital))
+        return 'Столица "{}" не найдена.'.format(capital)
